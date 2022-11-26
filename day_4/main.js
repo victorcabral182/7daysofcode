0be61dcc77670a47782659adc80e8e3a974c81d3
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2022-11-25 23:18:09 -0300

    conclusão do dia 4

---
+++ day_4/main.js
@@ -0,0 +1,25 @@
+alert('Tente adivinhar o número de 1 a 10 que estou pensando.');
+/////////////////////////////////////////////////////////////////
+let numeroX = Math.floor(Math.random() * (10 - 1 + 1) + 1)
+//const numeroX = 7;
+let tentativasMax = 3;
+let resposta = "";
+let acertou = false;
+//console.log(numeroX);
+/////////////////////////////////////////////////////////////////
+for (let index = 0; index < tentativasMax; index++) {
+
+	resposta = prompt('Escolha um número:');
+
+	if (resposta == numeroX) {
+		alert(`Parabéns você acertou, o número era ${numeroX}.`);
+		acertou = true;
+		break;
+	}
+
+	alert('Errado!');
+}
+
+if (!acertou) {
+	alert(`Você não conseguiu acertar, o número era: ${numeroX}.`);
+}
